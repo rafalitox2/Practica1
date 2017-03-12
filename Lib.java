fe34510b6805181abbe5841a680e4c57ae7964e5
rafalitox2 <rafaelhormigog@gmail.com>
2017-03-12 13:32:20 +0100

    modificaciones alumno 2

--- Lib.java
+++ Lib.java
@@ -11,9 +11,12 @@ public class Lib {
 
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+	   System.out.println("modificado por alumno 2"); // alumno 2
     }
 
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+	   // modificaciones alumno 2
+System.out.println("Alumno 2 inserta esta sentencia");
     }
 }
